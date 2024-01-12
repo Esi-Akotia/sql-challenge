ee0b5239c85eacd4f59c41397c5d9fa3ebad503b
Esi-Akotia <esi.akotia@gmail.com>
2024-01-12 17:25:45 -0500

    final analysis

--- EmployeeSQL/employee data analysis.sql
+++ EmployeeSQL/employee data analysis.sql
@@ -55,6 +55,11 @@ inner join dept_emp
 on dept_emp.emp_no = employees.emp_no
 join departments
 on dept_emp.dept_no = departments.dept_no
-where dept_emp.dept_no = 'd007'or dept_emp.dept_no = 'd005'
+where dept_emp.dept_no = 'd007' or dept_emp.dept_no = 'd005'
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+select employees.last_name
+,count(employees.last_name) as "frequency"
+from employees
+group by employees.last_name
+order by frequency desc
